358c7aa1d052f7c6014fa691f406ebebdfeff1bb
observaInc <dev-team@observanow.com>
2022-12-30 12:37:40 -0800

    More require changes

--- scripts/beforePrepare.js
+++ scripts/beforePrepare.js
@@ -1,7 +1,7 @@
 module.exports = function(context) {
   var fs = require('fs');
   var path = require('path');
-  var cordova_util = require('cordova-utils')
+  var cordova_util = context.requireCordovaModule('cordova-lib/src/cordova/util')
   var ConfigParser = context.requireCordovaModule('cordova-common').ConfigParser;
 
   var projectRoot = context.opts.projectRoot;
